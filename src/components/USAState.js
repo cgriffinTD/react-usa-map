d5f4cd7dcb75418bee65f57388991c3964b5a975
cgriffinTD <chance@turbodebt.com>
2023-01-13 09:27:32 -0800

    push

--- src/components/USAState.js
+++ src/components/USAState.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const USAState = (props) => {
   return (
-    <path d={props.dimensions} fill={props.fill} data-name={props.state} className={`${props.state} state`} onClick={props.onClickState}>
-      <title>{props.stateName}</title>
+    <path
+      d={props.dimensions}
+      fill={props.fill}
+      data-name={props.state}
+      className={`${props.state} state`}
+      onClick={props.onClickState}
+    >
+      <title>Debt Relief in {props.stateName}</title>
     </path>
   );
-}
+};
 export default USAState;
